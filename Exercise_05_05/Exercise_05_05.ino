ed06260df00cf846ceb295bb5f91f8b51c79f2ce
Daniel Eichhorn <dani.eichhorn@squix.ch>
2018-09-07 08:08:53 +0200

    Add exercise about reading the manual

---
+++ Exercise_05_05/Exercise_05_05.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>  
+#include "SSD1306Wire.h"
+
+SSD1306Wire  display(0x3c, D3, D4);
+
+void setup() {
+  // Initialize the driver
+  display.init();
+
+  // define font
+  display.setFont(ArialMT_Plain_10);
+
+  // add method here to center the text
+
+  
+  // Set the drawing color once
+  display.setColor(WHITE);
+}
+
+void loop() {
+  // Clear the frame buffer
+  display.clear();
+
+  String textToDisplay = "This is a very long text which should be aligned horizontally to the center of the screen and automatically wrapped to the next line.";
+  
+  // Find a method in the documentation which draws the above text with line wrap enabled
+
+
+  // Write the frame buffer to the display.
+  display.display();
+
+}
+
